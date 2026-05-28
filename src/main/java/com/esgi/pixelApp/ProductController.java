f6b5588d622b24f56693fdba3acc46d9e9a88783
Azizfassinou <azizfassinou4@gmail.com>
2026-05-28 12:43:08 +0200

    Base de l'API REST

---
+++ src/main/java/com/esgi/pixelApp/ProductController.java
@@ -0,0 +1,17 @@
+package com.esgi.pixelApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class ProductController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ src/test/java/com/esgi/pixelApp/PixelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgi.pixelApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
